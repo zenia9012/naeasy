027a23c7953aa520af96ad0c4c14a2735105999c
Yevhenii Riabyi <zenia9012@gmail.com>
2018-03-30 21:50:40 +0300

    add lang file

---
+++ resources/lang/ua/base.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+
+    'menu_service' => 'Послуги',
+    'menu_about' => 'Про нас',
+    'menu_kp' => 'Отримати комерційну пропозицію',
+    'menu_contact' => 'Контакти',
+
+
+//contact page
+
+	'our_contact' => 'Наші контакти',
+	'tel' => 'Телефон',
+	'email' => 'Email',
+
+	// contact form placeholder
+
+	'pl_name' => 'Введіть ім\'я',
+	'pl_email' => 'Введіть email',
+	'pl_message' => 'Введіть повідомлення',
+	'pl_button' => 'Вислати',
+	'contact_form_title' => 'Залиште своє повідомлення',
+	'contact_form_text' => 'Якщо у Вас виникли запитання або пропозиції, напишить нам про це, ми відповімо так швидко як зможемо'
+
+
+];
