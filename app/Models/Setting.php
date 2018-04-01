5f8c41a95237578f14a51652ac7a3fd2fe9f1f26
Yevhenii Riabyi <zenia9012@gmail.com>
2018-04-01 09:19:54 +0300

    add settings to admin section

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+	use CrudTrait;
+
+	protected $fillable = [
+		'title', 'key', 'value'
+	];
+
+	public static function getPhone1(  ) {
+		$result = Setting::where('key', 'phone_1')->first();
+
+		return $result->value;
+    }
+
+	public static function getPhone2(  ) {
+		$result = Setting::where('key', 'phone_2')->first();
+
+		return $result->value;
+	}
+
+	public static function getEmail(  ) {
+		$result = Setting::where('key', 'email')->first();
+
+		return $result->value;
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,27 @@
 <?php
 
 
-Route::get( '/','DefaultController@index' )->name('main');
-Route::get( '/about','DefaultController@about' )->name('about');
-Route::get( '/contact','DefaultController@contact' )->name('contact');
-Route::get( '/commercial','DefaultController@commercial' )->name('commercial');
-Route::get( '/services','DefaultController@services' )->name('services');
+Route::get( '/', 'DefaultController@index' )->name( 'main' );
+Route::get( '/about', 'DefaultController@about' )->name( 'about' );
+Route::get( '/contact', 'DefaultController@contact' )->name( 'contact' );
+Route::get( '/commercial', 'DefaultController@commercial' )->name( 'commercial' );
+Route::get( '/services', 'DefaultController@services' )->name( 'services' );
 
 // form
 
-Route::post('/feedback' , 'FormController@feedback')->name('feedback');
+Route::post( '/feedback', 'FormController@feedback' )->name( 'feedback' );
 
 
-
-Route::group( [ 'prefix'     => config( 'backpack.base.route_prefix' ),
-                'middleware' => [ 'admin' ],
-                'namespace'  => 'Admin'
+Route::group( [
+	'prefix'     => config( 'backpack.base.route_prefix' ),
+	'middleware' => [ 'admin' ],
+	'namespace'  => 'Admin'
 ], function () {
+
+	CRUD::resource( 'setting', 'SettingCrudController' );
 
 } );
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get( '/home', 'HomeController@index' )->name( 'home' );
