2902fad28dd29b97377f3cd1401446e93b6fa4ad
Yevhenii Riabyi <zenia9012@gmail.com>
2018-04-07 23:27:59 +0300

    update social block

--- resources/views/blocks/social.blade.php
+++ resources/views/blocks/social.blade.php
@@ -5,7 +5,6 @@
             <p class="">{{ __('about.social_text') }}</p>
             <div class="social_link">
                 <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
                 <a href="#"><i class="fab fa-telegram"></i></a>
                 <a href="#"><i class="fab fa-whatsapp"></i></a>
                 <a href="#"><i class="fab fa-instagram"></i></a>
