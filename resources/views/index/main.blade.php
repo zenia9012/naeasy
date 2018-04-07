df976798e829a9739faba0e188998c812ce3cb1c
Yevhenii Riabyi <zenia9012@gmail.com>
2018-04-07 17:33:56 +0300

    recognize main page

--- resources/views/index/main.blade.php
+++ resources/views/index/main.blade.php
@@ -4,8 +4,12 @@
 
     @include('blocks.main_image')
 
+    @include('includes.header')
+
     @include('blocks.services')
 
     @include('blocks.callback')
+
+    @include('blocks.social')
 
 @endsection
